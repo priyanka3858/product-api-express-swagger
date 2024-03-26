e0e26a40beae119390cb788d21dad5442b8fb39c
priyanka <priyanka3858@gmail.com>
2024-03-26 12:07:43 -0400

    API setup

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// GET all
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// GET single
+app.get("/:id", (req, res) => {
+  res.send("Hello World!");
+});
+
+// POST request handler
+app.post("/", (req, res) => {
+  res.send("Hello World from POST!");
+});
+
+// PUT request handler
+app.put("/:id", (req, res) => {
+  res.send("Hello World from PUT!");
+});
+
+// DELETE request handler
+app.delete("/:id", (req, res) => {
+  res.send("Hello World from DELETE!");
 });
 
 app.listen(port, () => {
